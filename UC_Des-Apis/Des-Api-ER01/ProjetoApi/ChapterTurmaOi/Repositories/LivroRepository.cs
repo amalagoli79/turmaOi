f1b59e2ff6fd22aa84e060b5fcae94dfaa9cb656
amalagoli79 <adriano.g.malagoli@gmail.com>
2022-12-07 21:45:55 -0300

    TurmaOi_ER02_UC17_Des.Api-07-12-22

---
+++ UC_Des-Apis/Des-Api-ER01/ProjetoApi/ChapterTurmaOi/Repositories/LivroRepository.cs
@@ -0,0 +1,26 @@
+﻿
+
+using ChapterTurmaOi.Contexts;
+using ChapterTurmaOi.Models;
+
+namespace ChapterTurmaOi.Repositories
+{
+    public class LivroRepository
+    {
+        private readonly Sqlcontext _context;
+        public LivroRepository(Sqlcontext context)
+        {
+            _context = context;
+
+        }
+
+        public List<Livro> Listar()
+        {
+
+            return _context.Livros.ToList();
+        }
+
+
+
+    }
+}
