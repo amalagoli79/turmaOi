beb58f65346c4a05bece25956040034dec2a938c
amalagoli79 <adriano.g.malagoli@gmail.com>
2022-12-12 19:41:34 -0300

    TurmaOi_ER02.C_UC17_Des.Api-12-12-22

---
+++ UC_Des-Apis/Des-Api-ER01/ProjetoApi/ChapterTurmaOi/Controllers/LivroController.cs
@@ -0,0 +1,109 @@
+﻿using ChapterTurmaOi.Models;
+using ChapterTurmaOi.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChapterTurmaOi.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class LivroController : ControllerBase
+    {
+        private readonly LivroRepository _livroRepository;
+
+        public LivroController(LivroRepository livroRepository)
+        {
+            _livroRepository = livroRepository;
+
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        /// <exception cref="Exception"></exception>
+        [HttpGet]
+        public IActionResult ler()
+        {
+            try
+            {
+                return Ok(_livroRepository.Listar());
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult BuscarPorId(int id)
+        {
+            try
+            {
+                Livro LivroBuscado = _livroRepository.BuscarPorId(id);
+
+                if (LivroBuscado == null)
+                {
+                    return NotFound();
+                }
+
+                return Ok(LivroBuscado);
+
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Casdastrar(Livro l)
+        {
+            try
+            {
+                _livroRepository.Cadastrar(l);
+                return StatusCode(201);
+                //return Ok("Livro cadastrado com sucesso");
+            }
+
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            try
+            {
+                _livroRepository.Deletar(id);
+                return Ok("Livro removido com sucesso");
+            }
+
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
+        [HttpPut("{id}")]
+
+        public IActionResult Altera(int id, Livro l)
+        {
+
+            try
+            {
+                _livroRepository.Alterar(id, l);
+                return StatusCode(204);
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+
+
+        }
+
+    }
+}
---
+++ UC_Des-Apis/Des-Api-ER02/ProjetoApi/ChapterTurmaOi/ChapterTurmaOi/Repositories/LivroRepository.cs
@@ -0,0 +1,59 @@
+﻿
+
+using ChapterTurmaOi.Contexts;
+using ChapterTurmaOi.Models;
+
+namespace ChapterTurmaOi.Repositories
+{
+    public class LivroRepository
+    {
+        private readonly Sqlcontext _context;
+        public LivroRepository(Sqlcontext context)
+        {
+            _context = context;
+
+        }
+
+        public List<Livro> Listar()
+        {
+
+            return _context.Livros.ToList();
+        }
+
+        public Livro BuscarPorId(int id)
+        {
+            return _context.Livros.Find(id);
+        }
+
+        public void Cadastro(Livro l)
+        {
+            _context.Livros.Add(l);
+            _context.SaveChanges();
+        }
+
+
+        public void Deletar(int id)
+        {
+            Livro l = _context.Livros.Find(id);
+            _context.Livros.Remove(l);
+            _context.SaveChanges();
+        }
+
+        public void Alterar(int id, Livro l)
+        {
+            Livro livroBuscado = _context.Livros.Find(id);
+
+            if (livroBuscado != null)
+            {
+                livroBuscado.Titulo = l.Titulo;
+                livroBuscado.QuantidadePaginas = l.QuantidadePaginas;
+                livroBuscado.Disponivel = l.Disponivel;
+
+                _context.Livros.Update(livroBuscado);
+                _context.SaveChanges();
+            }
+
+        }
+
+    }
+}
